a963adcc574aebc2344aa2f887252fab3677859e
RoshanAAS <roshan555routh@gmail.com>
2023-08-10 20:10:07 +0530

    new changes

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 // server.js
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const path = require('path')
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
 
-const cors=require("cors")
- server.use(cors())
+const cors = require('cors')
+
+
+
+
+server.use(cors())
+server.use(jsonServer.bodyParser)
 server.use(middlewares)
-server.use('', router)
-server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
+server.use(router)
+
+const PORT = 8000
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`)
 })
+
